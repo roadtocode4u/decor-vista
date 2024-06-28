1c098026a7ed014a30c974792af334a400076b97
Vaibhavihole31 <itspinki05@gmail.com>
2024-06-28 10:25:05 +0530

    Revert "Routing create"
    This reverts commit 0071944d0c9ddf479fd91198df740b7016fb749b.

--- src/index.js
+++ src/index.js
@@ -2,26 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './views/Home/Home';
-
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const router = createBrowserRouter([
-
-{
-  path:"/",
-  element:<Home/>
-},
-{
-  path:"/app",
-  element:<App/>
-}
-
-
-
-
-
-])
-root.render(<RouterProvider router={router}/> );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
