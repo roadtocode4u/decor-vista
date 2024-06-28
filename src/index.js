0071944d0c9ddf479fd91198df740b7016fb749b
lakshmankolpe <lakshmankolpe912@gmail.com>
2024-06-28 10:09:34 +0530

    Routing create

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './views/Home/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+
+{
+  path:"/",
+  element:<Home/>
+},
+{
+  path:"/app",
+  element:<App/>
+}
+
+
+
+
+
+])
+root.render(<RouterProvider router={router}/> );
 
