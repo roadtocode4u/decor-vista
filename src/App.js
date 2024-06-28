1d0b33cc3c5cabb64be85ca366a73d97388f8514
lakshmankolpe <lakshmankolpe912@gmail.com>
2024-06-28 12:31:37 +0530

    routing create

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client'; 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './views/Home/Home';
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>React App</h1>
-        {/* add routing here */}
-      </div>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
 
 export default App;
