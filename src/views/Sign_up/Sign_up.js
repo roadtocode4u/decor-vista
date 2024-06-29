b0069dd07e152e763552cd3af7099032c61b04af
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-06-29 17:50:18 +0530

    created sign page

--- src/views/Sign_up/Sign_up.js
+++ src/views/Sign_up/Sign_up.js
@@ -1,52 +1,38 @@
 import "./Sign_up.css"
-import img from "./images/sign-up.png"
-import gmail from "./images/gmail.png"
-import facebook from "./images/facebook.png"
-import twitter from "./images/twitter.png"
 import toast, { Toaster } from 'react-hot-toast'
 
-
 function Sign_up() {
-  return (<>
+     return (<>
+          <div className=" main-container d-flex flex-wrap">
+          <div className="signup-contaainer w-50 bg-secondary">
+               <h1 className="fs-1 text-center mt-4">
+                    "Designs that Spark Joy"
+               </h1>
+               <h2 className="fs-2 text-center mt-5">Sign-Up Here</h2>
 
-<div className="sign-in-container d-flex justify-content-evenly justify-content-center flex-wrap ">
-    <div className="img-container">
-<img src={img} className="signup-img rounded-4" />
-</div>
-<div className="sign-in-form-container rounded-4">
-
-    <h1 className=" text-center">Sign Up</h1>
-
-    <div className="icon-container d-flex align-items-center justify-content-evenly">
-        <img src={gmail} className="sign-icon" />
-        <img src={facebook} className="sign-icon" />
-        <img src={twitter} className="sign-icon" />
-    </div>
-
-    <div className="sign-form-container">
-        <form>
-            <input type="text" placeholder="Name" className="input-box px-3" />
-            <input type="email" placeholder="Email" className="input-box px-3" />
-            <input type="password" placeholder="password" className="input-box px-3" />
-        </form>
-    </div>
-
-    <div>
-        <button type="submit" className="signup-btn bg-primary"
-         onClick={
-            () => {
-              toast.success("You are  Successfully sign up!")
-            }
-          }>Sign up</button>
-    </div>
-</div>
-</div>
-<Toaster/> 
+               
+         <div className=" d-flex flex-column  w-75 mx-auto " >
+           <form>
+               <div className=" my-3 ">
+                 <h4 className="fs-5  ">Enter Full Name:</h4>   
+        
+             <input type="text" placeholder="Name" id="name" className="input-box rounded-2 px-3 py-1 w-100  fs-5 border-0" required/>
+             </div>
+             
+             </form>
+             
+         </div>
+          </div>
 
 
-  </>
 
-  )
+          <div className="w-50 bg-success">
+               <h1 className="fs-1">hello</h1>
+          </div>
+          </div>
+          <Toaster />
+
+     </>)
 }
 
 export default Sign_up
